4a4f32a533f776acfba12ffb4a1281204b34e01d
cubo3d <davisilvaperes3@gmail.com>
2025-08-10 20:25:13 -0300

    Facilitação no arquivo configuracão do programa

--- Recursos/config.py
+++ Recursos/config.py
@@ -9,23 +9,37 @@
 #
 # Versão descontinuado: false
 #
+# Perceba que tem partes em que nao é recomendado mexer! saiba mais na wiki
+#
 
 # theme: ""
+
 
 ###########
 ## CORES ##
 ###########
 
-background_color = "\033[38;2;30;30;30m"
-border_color = "\033[38;2;160;160;160m"
-text_color = "\033[38;2;255;255;255m"
+RB = 30
+GB = 30
+BB = 30
 
+RF = 255
+GF = 255
+BF = 255
+
+Rb = 160
+Gb = 160
+Bb = 160
+
+background_color = "\033[38;2;<RB>;<GB>;<BB>m"
+foreground_color = "\033[38;2;<RF>;<GF>;<BF>m"
+border_color = "\033[38;2;<Rb>;<Gb>;<Bb>m"
 
 ############
 ## FONTES ##
 ############
 
-font = "Ubuntu"
+gfont = "Ubuntu"
 font_size = 14
 
 
@@ -41,3 +55,14 @@ font_size = 14
 #
 #
 #
+
+
+#######################
+# CONFIGURAÇÃO OCULTA #
+#######################
+#
+# Nessa parte possuí que não pode ser modificado, caso contrário pode acontecer coisas inesperaveis no programa
+
+background_color = "\033[38;2;<RB>;<GB>;<BB>m"
+foreground_color = "\033[38;2;<RF>;<GF>;<BF>m"
+border_color = "\033[38;2;<Rb>;<Gb>;<Bb>m"
