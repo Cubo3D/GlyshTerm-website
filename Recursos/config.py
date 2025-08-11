be4df9aab96aaab13d65f2ef4a5613763de169d6
cubo3d <davisilvaperes3@gmail.com>
2025-08-10 22:43:27 -0300

    Modificação no arquivo de configuração

--- Recursos/config.py
+++ Recursos/config.py
@@ -4,65 +4,43 @@
 # Puxado automaticamente em "https://glysh-term.vercel.app/Recursos/config.py"
 #
 # Autor: Cubo3D
-# Data da ultima modificação: 
+# Data da ultima modificação:
 # Versão: 1.0
 #
 # Versão descontinuado: false
-#
-# Perceba que tem partes em que nao é recomendado mexer! saiba mais na wiki
 #
 
 # theme: ""
 
 
-###########
-## CORES ##
-###########
+# <<<<<<<<<<<
+# ^^ CORES ^^
+# >>>>>>>>>>>
 
-RB = 30
-GB = 30
-BB = 30
+background_color = 30, 30, 30
 
-RF = 255
-GF = 255
-BF = 255
+foreground_color = 255, 255, 255
 
-Rb = 160
-Gb = 160
-Bb = 160
+border_color = 160, 160, 160
 
-background_color = "\033[38;2;<RB>;<GB>;<BB>m"
-foreground_color = "\033[38;2;<RF>;<GF>;<BF>m"
-border_color = "\033[38;2;<Rb>;<Gb>;<Bb>m"
 
-############
-## FONTES ##
-############
+# >>>>>>>>>>>>
+# ^^ FONTES ^^
+# <<<<<<<<<<<<
 
-gfont = "Ubuntu"
+font = "JetBrains Mono Nerd Font"
 font_size = 14
 
 
-######################
-## ATALHOS DE TECLA ##
-######################
+# <<<<<<<<<<<<<<<<<<<<<<<<
+# ^^ ATALHOS DO TECLADO ^^
+# >>>>>>>>>>>>>>>>>>>>>>>>
 #
 #
 #
-#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-# Não suportado no momento, mas tentarei trazer na versão 1.5^
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>^
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#  Não suportado no momento, mas tentarei trazer na versão 1.5 ^
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>^
 #
 #
 #
-
-
-#######################
-# CONFIGURAÇÃO OCULTA #
-#######################
-#
-# Nessa parte possuí que não pode ser modificado, caso contrário pode acontecer coisas inesperaveis no programa
-
-background_color = "\033[38;2;<RB>;<GB>;<BB>m"
-foreground_color = "\033[38;2;<RF>;<GF>;<BF>m"
-border_color = "\033[38;2;<Rb>;<Gb>;<Bb>m"
